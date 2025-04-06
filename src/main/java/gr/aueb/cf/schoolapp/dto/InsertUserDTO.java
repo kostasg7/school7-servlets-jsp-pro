2f87643541b559d10210dd5d2c381f4209238ee9
K. Papadopoulos <kostashope@hotmail.com>
2025-04-06 11:40:47 +0300

    feat: dto, dbutil & validator created

---
+++ src/main/java/gr/aueb/cf/schoolapp/dto/InsertUserDTO.java
@@ -0,0 +1,21 @@
+package gr.aueb.cf.schoolapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class InsertUserDTO extends BaseUserDTO {
+
+    private String role;
+
+    public InsertUserDTO(String username, String password, String confirmedPassword, String role) {
+        super(username, password, confirmedPassword);
+        this.role = role;
+    }
+
+}
---
+++ src/main/java/gr/aueb/cf/schoolapp/dto/UserReadOnlyDTO.java
@@ -0,0 +1,21 @@
+package gr.aueb.cf.schoolapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class UserReadOnlyDTO extends BaseUserDTO {
+
+    private Integer id;
+
+    public UserReadOnlyDTO() {}
+
+    public UserReadOnlyDTO(Integer id, String username, String password) {
+        super(username, password);
+        this.id = id;
+    }
+
+}
